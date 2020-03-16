796e3cbf654fceb5445d8c172a48e0016ac70fd4
alirezakohandani <alireza.ko73@gmail.com>
2020-03-16 18:22:06 +0330

    commit on master https://github.com/alirezakohandani/personalll.git

--- gallery.php
+++ gallery.php
@@ -22,44 +22,8 @@
     <body>
         
         <!--================Header Menu Area =================-->
-        <header class="header_area">
-            <div class="main_menu">
-            	<nav class="navbar navbar-expand-lg navbar-light">
-					<div class="container box_1620">
-						<!-- Brand and toggle get grouped for better mobile display -->
-						<a class="navbar-brand logo_h" href="index.html"><img src="img/logo.png" alt=""></a>
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<!-- Collect the nav links, forms, and other content for toggling -->
-						<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-							<ul class="nav navbar-nav menu_nav ml-auto">
-								<li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li> 
-								<li class="nav-item"><a class="nav-link" href="about-us.html">About</a></li> 
-								<li class="nav-item"><a class="nav-link" href="services.html">Services</a></li> 
-								<li class="nav-item submenu dropdown">
-									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
-									<ul class="dropdown-menu">
-										<li class="nav-item"><a class="nav-link" href="portfolio.html">Portfolio</a></li>
-										<li class="nav-item"><a class="nav-link" href="elements.html">Elements</a></li>
-									</ul>
-								</li> 
-								<li class="nav-item submenu dropdown">
-									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
-									<ul class="dropdown-menu">
-										<li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
-										<li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
-									</ul>
-								</li> 
-								<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
-							</ul>
-						</div> 
-					</div>
-            	</nav>
-            </div>
-        </header>
+       <?php include 'header.php' ?>
+					
         <!--================Header Menu Area =================-->
         
         <!--================Home Banner Area =================-->
@@ -82,7 +46,7 @@
         
         
         <!-- Start Sample Area -->
-			<section class="sample-text-area">
+			<!-- <section class="sample-text-area">
 				<div class="container">
 					<h3 class="text-heading title_color">Text Sample</h3>
 					<p class="sample-text">
@@ -90,10 +54,10 @@
 
 					</p>
 				</div>
-			</section>
+			</section> -->
 			<!-- End Sample Area -->
 			<!-- Start Button -->
-			<section class="button-area">
+			<!-- <section class="button-area">
 				<div class="container border-top-generic">
 					<h3 class="text-heading title_color">Sample Buttons</h3>
 					<div class="button-group-area">
@@ -187,10 +151,10 @@
 						<a href="#" class="genric-btn primary-border small">Small</a>
 					</div>
 				</div>
-			</section>
+			</section> -->
 			<!-- End Button -->
 			<!-- Start Align Area -->
-			<div class="whole-wrap">
+			<!-- <div class="whole-wrap">
 				<div class="container">
 					<div class="section-top-border">
 						<h3 class="mb-30 title_color">Left Aligned</h3>
@@ -340,14 +304,58 @@
 								</div>
 							</div>
 						</div>
-					</div>
-					<div class="section-top-border">
+					</div> -->
+		     	 	<div class="section-top-border">
 						<h3 class="title_color">Image Gallery</h3>
 						<div class="row gallery-item">
 							<div class="col-md-4">
-								<a href="img/elements/g1.jpg" class="img-gal"><div class="single-gallery-image" style="background: url(img/elements/g1.jpg);"></div></a>
+							<?php
+							include 'connect.php';
+
+
+							$select = $con->prepare("SELECT * FROM news");
+							$select->setFetchMode(PDO::FETCH_ASSOC);
+							$select->execute();
+							while ($data = $select->fetch()) {
+								$image = $data['image'];
+								$id = $data['id'];
+								echo "<img src='newsuploads/$image'>";
+								echo "<form method='POST'><input type='text' name='comment' placeholder='comment'><input type='submit' value='submit' name='ok'></form>";
+								$comment = $_POST["comment"];
+								if (isset($_POST["ok"])) {
+										
+									
+									
+										$sql = "INSERT INTO commen1 (commentt, id)VALUES ('$comment', '$id')";
+										if ($con->query($sql)) {
+											//   echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+												echo "yes";
+										}
+											else{
+											echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+											}
+									
+									
+									
+
+         					 $pdo = null;
+							}
+								?>
+								<p><?php 
+								include 'connect.php';
+								
+								}
+								$select = $con->prepare("SELECT * FROM comment");
+								$select->setFetchMode(PDO::FETCH_ASSOC);
+								$select->execute();
+								while ($data = $select->fetch()) {
+									echo $data['commenttext'] ,"<br>";
+								}
+								?></p>
+								
+							</form>
 							</div>
-							<div class="col-md-4">
+							<!-- <div class="col-md-4">
 								<a href="img/elements/g2.jpg" class="img-gal"><div class="single-gallery-image" style="background: url(img/elements/g2.jpg);"></div></a>
 							</div>
 							<div class="col-md-4">
@@ -367,10 +375,10 @@
 							</div>
 							<div class="col-md-4">
 								<a href="img/elements/g8.jpg" class="img-gal"><div class="single-gallery-image" style="background: url(img/elements/g8.jpg);"></div></a>
-							</div>
+							</div> -->
 						</div>
 					</div>
-					<div class="section-top-border">
+					<!-- <div class="section-top-border">
 						<div class="row">
 							<div class="col-lg-4 col-sm-6 typo-sec">
 								<h3 class="mb-20 title_color">Typography</h3>
@@ -604,59 +612,13 @@
 						</div>
 					</div>
 				</div>
-			</div>
+			</div> -->
 			<!-- End Align Area -->
         
         
         <!--================Footer Area =================-->
-        <footer class="footer_area p_120">
-        	<div class="container">
-        		<div class="row footer_inner">
-        			<div class="col-lg-5 col-sm-6">
-        				<aside class="f_widget ab_widget">
-        					<div class="f_title">
-        						<h3>About Me</h3>
-        					</div>
-        					<p>Do you want to be even more successful? Learn to love learning and growth. The more effort you put into improving your skills,</p>
-        					<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-        				</aside>
-        			</div>
-        			<div class="col-lg-5 col-sm-6">
-        				<aside class="f_widget news_widget">
-        					<div class="f_title">
-        						<h3>Newsletter</h3>
-        					</div>
-        					<p>Stay updated with our latest trends</p>
-        					<div id="mc_embed_signup">
-                                <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative">
-                                	<div class="input-group d-flex flex-row">
-                                        <input name="EMAIL" placeholder="Enter email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
-                                        <button class="btn sub-btn"><span class="lnr lnr-arrow-right"></span></button>		
-                                    </div>				
-                                    <div class="mt-10 info"></div>
-                                </form>
-                            </div>
-        				</aside>
-        			</div>
-        			<div class="col-lg-2">
-        				<aside class="f_widget social_widget">
-        					<div class="f_title">
-        						<h3>Follow Me</h3>
-        					</div>
-        					<p>Let us be social</p>
-        					<ul class="list">
-        						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-        						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-        						<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-        						<li><a href="#"><i class="fa fa-behance"></i></a></li>
-        					</ul>
-        				</aside>
-        			</div>
-        		</div>
-        	</div>
-        </footer>
+		<?php include 'footer.php'; ?>		
+		
         <!--================End Footer Area =================-->
         
         
