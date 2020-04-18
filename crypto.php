8de7e1d5bb55d8a99cd04bc8f3de6047aa162c30
alirezakohandani <alireza.ko73@gmail.com>
2020-04-18 16:52:43 +0430

    add other cryptocurrency price such as ETH,REP,DASH,XRP, ...

--- crypto.php
+++ crypto.php
@@ -1,37 +1,59 @@
-
+<link rel="stylesheet" href="css/bootstrap.css">
+      
 <?php
-$url = "https://api.coinbase.com/v2/prices/spot?currency=USD";
+
+$url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,REP,DASH,XRP,EOS,NEO&tsyms=USD,EUR";
 $result = file_get_contents($url);
 $r = json_decode($result);
-foreach($r as $key => $value):?>
+?>
+<section>
+    <div class="row">
+        <div class="container">
+            <div class="col-md-12">
+                <h2 style="text-align: center">cryptocurrency price</h2>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
 <div class="row">
 <div class="container">
-    <div class="col-md-12">
+<div class="col-md-12">
 <table style="width: 100%" class="table table-striped table-dark">
-<thead class="thead-dark">
+<thead style="text-align: center" class="thead-dark">
 <tr>
-<th scope="col">#</th>
-<th scope="col">image</th>
-<th scope="col">name</th>
-<th scope="col">price</th>
+<th style="color: gray" scope="col">row</th>
+<!-- <th style="color: gray" scope="col">image</th> -->
+<th style="color: gray" scope="col">name</th>
+<th style="color: gray" scope="col">price USD</th>
+<th style="color: gray" scope="col">price EUR</th>
 </tr>
 </thead>
+
+<?php foreach($r as $key => $value):?>
 <tbody>
-    <tr>
-<th style="text-align: center" scope="row">1</th>
-<td ><img style="width: 30px; height: 30px;display: block; margin-left: auto; margin-right: auto; " src="img/bitcoin1.png"></td>
-<td style="text-align: center">bitcoin</td>
-<td style="text-align: center"><?= $value->amount; ?></td>
+<tr>
+<td style="text-align: center" scope="row">
+<?php $a[] = $key;
+echo sizeof($a);
+?></td>
+<!-- <td ><img style="width: 30px; height: 30px;display: block; margin-left: auto; margin-right: auto; " src="img/bitcoin1.png"></td> -->
+<td style="text-align: center"><?php echo $key; ?></td>
+<td style="text-align: center; font-weight: bold; color: green"><?= $value->USD; ?></td>
+<td style="text-align: center; font-weight: bold; color: green"><?= $value->EUR; ?></td>
     </tr>
 </tbody>
-</table>
-</div>
-</div>
-</div>
 <?php
 endforeach;
 
 ?>
+</table>
+</div>
+</div>
+</div>
+</section>
+
+
 
 
 
